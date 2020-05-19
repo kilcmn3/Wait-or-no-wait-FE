6dd64b808c94d107c49d1686053c44e75f2d20e2
kilcmn <kilcmn3@hotmail.com>
2020-05-19 14:45:21 -0400

    working on serialization from BE

--- src/actions/fetchDatas.js
+++ src/actions/fetchDatas.js
@@ -29,6 +29,7 @@ export const fetchWaitLists = () => {
 };
 
 export const postCustomer = (data) => {
+  console.log(data);
   return (dispatch) => {
     fetch(URL + '/customers', {
       method: 'POST',
@@ -47,6 +48,33 @@ export const postCustomer = (data) => {
       });
   };
 };
+
+// fetch('http://localhost:3000/favorites')
+// .then(resp => resp.json())
+// .then(list => {
+//     // List[Promise] -> Promise[List]
+//     // loop through each of the list in favorites
+//     return Promise.all(list.map(l => {
+//         const {user_id, project_id, id} = l
+//         // console.log(id ,"fav_id")
+//         // fetch both user and projectId
+//         const fetchUser = fetch(`http://localhost:3000/users/${user_id}`)
+//         const fetchProject = fetch(`http://localhost:3000/projects/${project_id}`)
+//         return Promise.all([fetchUser,fetchProject])
+//         .then(([userJson, projectJson]) => {
+//             // get the payload result in Json
+//             return Promise.all([userJson.json(), projectJson.json()])
+//         })
+//         // aggregate the user, project inside a single object
+//         .then(([user, project]) => {
+//             // console.log(user, project)
+//             return {
+//                 id,
+//                 user: { ...user },
+//                 project: {...project}
+//             }
+//         })
+//     }))
 
 // export const postWaitList = () => {
 //   let waitlist = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
