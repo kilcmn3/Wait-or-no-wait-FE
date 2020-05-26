c1287d30c3b0674f7711d6ec2540c5295db33489
kilcmn <kilcmn3@hotmail.com>
2020-05-26 17:49:18 -0400

    done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,84 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import Box from '@material-ui/core/Box';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
+import DateRangeIcon from '@material-ui/icons/DateRange';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Typography from '@material-ui/core/Typography';
+import SettingsIcon from '@material-ui/icons/Settings';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+
+const moment = require('moment');
 
 const useStyles = makeStyles((theme) => ({
-  // necessary for content to be below app bar
-  toolbar: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.background.default,
-    padding: theme.spacing(3),
+  list: {
+    padding: theme.spacing(4),
   },
 }));
 
 const Navbar = (props) => {
   const classes = useStyles();
 
+  // ['Home', 'Reservations', 'Customers', 'Profile', 'Logout'];
   return (
-    <div>
-      <div className={classes.toolbar} />
+    <Fragment>
+      <Typography component='div'>
+        <Box textAlign='center' fontWeight='fontWeightMedium' m={1}>
+          <Box>{moment(new Date()).format('MMMM Do , h:mm a')}</Box>
+        </Box>
+      </Typography>
       <Divider />
       <List>
-        {['Home', 'Reservations', 'Customers', 'Profile', 'Logout'].map(
-          (text, index) => (
-            <NavLink key={index} to={`/${text.toLowerCase()}`}>
-              <ListItem button key={text}>
-                <ListItemText primary={text} />
-              </ListItem>
-            </NavLink>
-          )
-        )}
+        <ListItem button className={classes.list} href='/'>
+          <ListItemIcon>
+            <AssignmentIcon color='primary' style={{ fontSize: 35 }} />
+          </ListItemIcon>
+          <ListItemText primary='Home' />
+        </ListItem>
+        <ListItem
+          button
+          className={classes.list}
+          component={Link}
+          to='/reservations'>
+          <ListItemIcon>
+            <DateRangeIcon color='primary' style={{ fontSize: 35 }} />
+          </ListItemIcon>
+          <ListItemText primary='Reservations' />
+        </ListItem>
+        <ListItem
+          button
+          className={classes.list}
+          component={Link}
+          to='/customers'>
+          <ListItemIcon>
+            <SupervisorAccountIcon color='primary' style={{ fontSize: 35 }} />
+          </ListItemIcon>
+          <ListItemText primary='Customers' />
+        </ListItem>
+        <ListItem
+          button
+          className={classes.list}
+          component={Link}
+          to='/profile'>
+          <ListItemIcon>
+            <SettingsIcon color='primary' style={{ fontSize: 35 }} />
+          </ListItemIcon>
+          <ListItemText primary='Profile' />
+        </ListItem>
+        <ListItem button className={classes.list} component={Link} to='/logout'>
+          <ListItemIcon>
+            <ExitToAppIcon color='primary' style={{ fontSize: 35 }} />
+          </ListItemIcon>
+          <ListItemText primary='Logout' />
+        </ListItem>
       </List>
-    </div>
+    </Fragment>
   );
 };
 
