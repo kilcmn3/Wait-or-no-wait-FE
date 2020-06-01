f2f7e7dbe31bcf02887bcc1f062ce9f97442e40d
kilcmn <kilcmn3@hotmail.com>
2020-06-01 18:09:01 -0400

    set time interval

--- src/components/WaitListRow.js
+++ src/components/WaitListRow.js
@@ -158,7 +158,7 @@ const WaitListRow = (props) => {
           });
         })
         .then(() => setTimer((timer) => !timer));
-    }, 60000);
+    }, 20000);
 
     return () => {
       window.clearInterval(timer);
